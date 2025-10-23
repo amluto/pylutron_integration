c69f3d9c9b162a064c837ff6dc1fa6929a2703ac
Andy Lutomirski <luto@kernel.org>
2025-10-23 16:53:49 -0700

    Start implementing read_unsolicited().  It's still rather buggy.
    Signed-off-by: Andy Lutomirski <luto@kernel.org>

--- pylutron_integration_standalone/connection.py
+++ pylutron_integration_standalone/connection.py
@@ -1,6 +1,7 @@
 import asyncio
 from dataclasses import dataclass
 import re
+import collections
 
 class LoginError(Exception):
     """Exception raised when login fails."""
@@ -33,14 +34,28 @@ class _Conn:
 class LutronConnection:
     """Represents an established Lutron connection."""
 
-    # TDB: a bit of a state machine is needed.  And we need to deal with the MONITORING config
-    # and the fact that the protocol looks different depending on the prompt and reply modes.
-
-    __conn: _Conn | None
+    __lock: asyncio.Lock
+    __conn: _Conn | None  # TODO: There is no value to ever nulling this out
     __prompt_prefix: bytes
+
+    # We can only handle one query and one read_unsolicited at a time.
+    __unsolicited_lock: asyncio.Lock
+    __query_lock: asyncio.Lock
+
+    # Deadlock prevention rules: __lock nests inside __query_lock, which nests inside __unsolicited_lock.
+    # Also, read_unsolicited() may not hold __query_lock while waiting for a message to arrive, as otherwise
+    # a query could be forced to wait forever if there aren't any messages.
+
+    __unsolicited_task: asyncio.Task[bytes] | None # protected by __lock
+    __unsolicited_queue: collections.deque[bytes]
     
     def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
         self.__conn = _Conn(reader, writer)
+        self.__unsolicited_lock = asyncio.Lock()
+        self.__query_lock = asyncio.Lock()
+        self.__lock = asyncio.Lock()
+        self.__unsolicited_task = None
+        self.__unsolicited_queue = collections.deque()
 
     @classmethod
     async def create_from_connnection(cls, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> 'LutronConnection':
@@ -75,14 +90,64 @@ class LutronConnection:
         return self
     
     async def raw_query(self, command: bytes) -> bytes:
-        assert self.__conn is not None
-        assert b'\r\n' not in command
+        async with self.__query_lock:
+            assert self.__conn is not None
+            assert b'\r\n' not in command
 
-        self.__conn.w.write(command + b'\r\n')
-        await self.__conn.w.drain()
+            # Pause any concurrent read_unsolicited
+            async with self.__lock:
+                if self.__unsolicited_task is not None:
+                    self.__unsolicited_task.cancel()
+                # and it won't resume because we're holding __query_lock
+
+            self.__conn.w.write(command + b'\r\n')
+            await self.__conn.w.drain()
     
-        reply = await self.__conn.r.readuntil(self.__prompt_prefix + b'>')
-        return reply[:-(len(self.__prompt_prefix) + 1)]
+            reply = await self.__conn.r.readuntil(self.__prompt_prefix + b'>')
+            # TODO: Actually parse out unsolicited messages
+            # Worse TODO: The QSE-CI-NWK-E helpfully sends b'QSE>' after every
+            # unsolicited monitoring message.  This means that we cannot rely on
+            # QSE> indicating the end of a query reply, and it also means we need
+            # to strip it after unsolicited monitoring messages.
+            #
+            # So far, the only way I've thought of to reliably parse multiline
+            # synchronous responses is to wait for the first line and then look
+            # for 'b'\r\nQSE>'
+            #
+            # Most likely this means that we will need to strictly distinguish between
+            # solicited and unsolicited messages by prefix, which will either require
+            # a global table or extra parameters to raw_query.
+            #
+            # Hmm, what do we do about the extra QSE> that will appear right after
+            # a command that gets no reply?
+            return reply[:-(len(self.__prompt_prefix) + 1)]
+        
+    # Reads one single unsolicited message
+    async def read_unsolicited(self) -> bytes:
+        assert self.__conn is not None
+
+        while True:
+            async with self.__query_lock:
+                # Why did we take the lock?  For two reasons:
+                # 1. If there is a query in progress, we need to wait for it to finish.
+                # 2. It protects __unsolicited_queue.
+                if len(self.__unsolicited_queue):
+                    return self.__unsolicited_queue.popleft()
+
+                async with self.__lock:
+                    self.__unsolicited_task = asyncio.create_task(self.__conn.r.readuntil(b'\r\n'))
+
+            try:
+                result = await self.__unsolicited_task
+            except asyncio.CancelledError:
+                async with self.__lock:
+                    self.__unsolicited_task = None
+                    continue # Try again
+
+            async with self.__lock:
+                self.__unsolicited_task = None
+
+            return result
 
     async def disconnect(self) -> None:
         if self.__conn is None:
