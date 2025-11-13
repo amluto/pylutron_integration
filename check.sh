e216f0a69c4ad86f079afbebe14c74305b85c547
Andy Lutomirski <luto@kernel.org>
2025-11-13 15:19:46 -0800

    Tidy up dev dependencies
    They all (IMO) logically belong to the workspace root, as they are
    used by people developing in the workspace.
    
    (The guidance for best practices for workspaces is pretty limited.
     I'll change this if something better comes up.)
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>

--- check.sh
+++ check.sh
@@ -5,7 +5,7 @@ echo "Running ruff..."
 uv run ruff check
 
 echo "Running mypy..."
-uv run mypy packages/pylutron-integration/src packages/pylutron-integration-cli/src
+uv run mypy packages/
 
 echo "Running tests..."
 uv run pytest packages/*/tests -v
